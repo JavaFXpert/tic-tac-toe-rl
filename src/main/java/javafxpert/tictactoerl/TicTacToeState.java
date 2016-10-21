2e07e5c42657c9c135cd7a418071666e9093fd8d
JavaFXpert <james@j1w.xyz>
2016-10-21 11:39:58 -0400

    Defined TicTacToeState class

--- src/main/java/javafxpert/tictactoerl/TicTacToeState.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeState.java
@@ -2,9 +2,12 @@ package javafxpert.tictactoerl;
 
 import burlap.mdp.core.state.MutableState;
 import burlap.mdp.core.state.State;
+import burlap.mdp.core.state.StateUtilities;
+import burlap.mdp.core.state.UnknownKeyException;
 import burlap.mdp.core.state.annotations.DeepCopyState;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,10 +15,15 @@ import java.util.List;
  */
 @DeepCopyState
 public class TicTacToeState implements MutableState {
+  private static String VAR_PRIOR_MOVES = "priorMoves";
+  private static String VAR_GAME_STATUS = "gameStatus";
+  private static String VAR_FIRST_PLAYER = "firstPlayer";
+
   private static String GAME_STATUS_IN_PROGRESS = "I";
   private static String GAME_STATUS_X_WON =       "X";
   private static String GAME_STATUS_O_WON =       "O";
   private static String GAME_STATUS_CATS_GAME =   "C";
+
   private static String FIRST_PLAYER_X =          "X";
 
   /**
@@ -43,21 +51,61 @@ public class TicTacToeState implements MutableState {
    */
   private String firstPlayer = FIRST_PLAYER_X;
 
+  private final static List<Object> keys =
+      Arrays.asList(VAR_PRIOR_MOVES, VAR_GAME_STATUS, VAR_FIRST_PLAYER);
 
-
-  public MutableState set(Object o, Object o1) {
-    return null;
+  public TicTacToeState() {
   }
 
+  public TicTacToeState(List<Integer> priorMoves, String gameStatus, String firstPlayer) {
+    this.priorMoves = priorMoves;
+    this.gameStatus = gameStatus;
+    this.firstPlayer = firstPlayer;
+  }
+
+  @Override
+  public MutableState set(Object variableKey, Object value) {
+    if(variableKey.equals(VAR_PRIOR_MOVES)){
+      this.priorMoves = (List<Integer>)value;
+    }
+    else if(variableKey.equals(VAR_GAME_STATUS)){
+      this.gameStatus = (String)value;
+    }
+    else if(variableKey.equals(VAR_FIRST_PLAYER)){
+      this.firstPlayer = (String)value;
+    }
+    else{
+      throw new UnknownKeyException(variableKey);
+    }
+    return this;
+  }
+
+  @Override
   public List<Object> variableKeys() {
-    return null;
+    return keys;
   }
 
-  public Object get(Object o) {
-    return null;
+  @Override
+  public Object get(Object variableKey) {
+    if(variableKey.equals(VAR_PRIOR_MOVES)){
+      return this.priorMoves;
+    }
+    else if(variableKey.equals(VAR_GAME_STATUS)){
+      return this.gameStatus;
+    }
+    else if(variableKey.equals(VAR_FIRST_PLAYER)){
+      return this.firstPlayer;
+    }
+    throw new UnknownKeyException(variableKey);
   }
 
-  public State copy() {
-    return null;
+  @Override
+  public TicTacToeState copy() {
+    return new TicTacToeState(priorMoves, gameStatus, firstPlayer);
+  }
+
+  @Override
+  public String toString() {
+    return StateUtilities.stateToString(this);
   }
 }
