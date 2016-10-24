b135aee58e4ef6e902610a10f734e39effcde4da
JavaFXpert <james@j1w.xyz>
2016-10-24 16:27:00 +0200

    Add Apache 2 license

--- src/main/java/javafxpert/tictactoerl/TicTacToeWorld.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeWorld.java
@@ -1,11 +1,17 @@
 /*
-  Development choices:
-  [] Implement an ActionType (because pre-conditions are required)
-
-  Development questions:
-  [] Use SimpleAction?
-  [] Use SampleModel or Enviromment?
-
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package javafxpert.tictactoerl;
 
@@ -22,7 +28,7 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
 /**
- * Created by jamesweaver on 10/20/16.
+ * @author James L. Weaver (Twitter: @JavaFXpert)
  */
 public class TicTacToeWorld implements DomainGenerator {
   @Override
